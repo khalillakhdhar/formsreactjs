13fdae89c539c17e2e05272ac06e420acdf9e141
khalil <khalillakhdharatc@gmail.com>
2022-09-20 11:24:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+class App extends React.Component{
+constructor(props)
+{
+    super(props);
+    this.state = {
+        nom:'',
+        prenom:''
+    }
+    this.handleSubmit=this.handleSubmit.bind(this);
+}
+onChange= e=> {
+    this.setState({ [e.target.name] :e.target.value});
+};
+handleSubmit(event)
+{
+    alert("Bonjour "+this.state.nom+" "+ this.state.prenom);
+    event.preventDefault();
+}
+render(){
+    return(
+        <div>
+            <form onSubmit={this.handleSubmit}>
+                <h1>Profil de {this.state.nom+" "+this.state.prenom}</h1>
+                Nom:<input type="text" value={this.state.nom} onChange={this.onChange} name="nom"></input>
+                Prenom:<input type="text" value={this.state.prenom} name="prenom" onChange={this.onChange}></input>
+                <button type="submit" >Afficher message</button>
+            </form>
+        </div>);
 }
 
-export default App;
+
+
+}
+export default App
